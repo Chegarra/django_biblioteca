9d2aa9c9a30b87d9f2bedd179129461a0bc6d5bf
Chema Segarra <chema.segarra@pfsgroup.es>
2022-10-20 23:38:15 +0200

    Añadido el componente 'Genero'

--- biblioteca/applications/genero/models.py
+++ biblioteca/applications/genero/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Genero(models.Model):
+    nombre = models.CharField('Género', max_length=32, unique=True)
+    codigo = models.CharField('Código', max_length=16, unique=True, help_text='XXXX-YYYY')
+
+
+    class Meta:
+        verbose_name = 'Género'
+        verbose_name_plural = 'Géneros'
+        ordering = ['nombre', 'codigo']
+
+
+    def __str__(self):
+        return f'{self.nombre} - ({self.codigo})'
