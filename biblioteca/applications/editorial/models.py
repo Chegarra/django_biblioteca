8248873b16c944d67dc9ef67ad21ce1ddd2d881d
Chema Segarra <chema.segarra@pfsgroup.es>
2022-10-20 23:37:19 +0200

    Añadido el componente 'Editorial'

--- biblioteca/applications/editorial/models.py
+++ biblioteca/applications/editorial/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Editorial(models.Model):
+
+    nombre = models.CharField('Editorial', max_length=32, unique=True)
+    pais = models.CharField('País', max_length=16, blank=True, help_text='País de Origen')
+    web = models.URLField('Página Web', max_length=256, blank=True)
+
+
+    class Meta:
+        verbose_name = 'Editorial'
+        verbose_name_plural = 'Editoriales'
+        ordering = ['nombre']
+
+
+    def __str__(self):
+        return f'{self.nombre}'
