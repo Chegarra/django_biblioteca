49a97bc8a2d54205e1fdb4372f3cb0ebc3ce501f
Chema Segarra <chema.segarra@pfsgroup.es>
2022-10-21 22:43:22 +0200

    Se añade el Autor con sus vistas y templates

--- biblioteca/applications/autor/urls.py
+++ biblioteca/applications/autor/urls.py
@@ -1,29 +1,37 @@
 from django.urls import path
 from . import views
 
-
 app_name = 'autor_app'
 
 urlpatterns = [
     path(
-        'autor/add', 
-        views.AutorCreateView.as_view()
+        'autor/inicio', 
+        views.AutorInicioTemplateView.as_view(), 
+        name='inicio'
         ),
     path(
-        'autor/success', 
-        views.SuccessView.as_view(), 
-        name='correcto'
+        'autor/lista',
+        views.ListAllAutores.as_view(), 
+        name='listar'
+        ),
+    path(
+        'autor/detalles/<pk>/', 
+        views.AutorDetailView.as_view(),
+        name='detalles'
+        ),
+    path(
+        'autor/add',
+        views.AutorCreateView.as_view(), 
+        name='add'
         ),
     path(
         'autor/edit/<pk>/', 
         views.AutorUpdateView.as_view(), 
-        name='editar_autor'
+        name='edit'
         ),
     path(
         'autor/delete/<pk>/', 
         views.AutorDeleteView.as_view(), 
-        name='borrar_autor'
-        ),    
-
+        name='delete'
+        )
 ]
-
--- biblioteca/applications/autor/views.py
+++ biblioteca/applications/autor/views.py
@@ -1,50 +1,59 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, TemplateView, UpdateView, DeleteView
 
-
+from django.views.generic import ListView, DetailView, TemplateView, CreateView, UpdateView, DeleteView
 from .models import Autor
-from .forms import *
 
-class AutorCreateView(CreateView):
+
+# Inicio para la Ubicación :: inicio
+class AutorInicioTemplateView(TemplateView):
+    template_name = 'autor/inicio.html'
+
+
+# Listar todos los Autores :: listar
+class ListAllAutores(ListView):
+    template_name = 'autor/listar_autores.html'
+    paginate_by = 12
+    ordering = 'nombre'
+    context_object_name = 'autor'
+
+    def get_queryset(self):
+        palabra_clave = self.request.GET.get('kword', '')
+        
+        lista = Autor.objects.filter(
+            nombre__icontains=palabra_clave
+        )
+        return lista
+
+
+# Ver la información del autor :: detalles
+class AutorDetailView(DetailView):
     model = Autor
-    template_name = "autor/add_autor.html"
-    #  fields = ['nombre', 'apellidos']
-    form_class = AutorCreateForm
-    success_url = reverse_lazy('autor_app:correcto')
+    template_name = 'autor/ver_autor.html'
 
-    # def form_valid(self, form):
-    #     autor = form.save()
-    #     nombre_completo = f'{autor.nombre} {autor.apellidos}'
-    #     print(autor)
-    #     print(nombre_completo)        
+
+# Añadir un nuevo autor :: add
+class AutorCreateView(CreateView):
+    template_name = "autor/add_autor.html"
+    model = Autor
+    fields = ('__all__')
+    success_url = reverse_lazy('autor_app:listar')
+
+    # def form_valid(self, form):      
     #     return super().form_valid(form)
 
 
-class SuccessView(TemplateView):
-    template_name = 'autor/success.html'
-
-
+# Modificar el autor :: edit
 class AutorUpdateView(UpdateView):
     template_name = 'autor/edit_autor.html'
     model = Autor
     fields = ('__all__')
-    success_url = reverse_lazy('autor_app:correcto')
-
-    # def post(self, requests, *args, **kwargs):
-    #     self.object = self.get_object()
-    #     print('************** POST **************')
-    #     print(requests.POST)
-    #     return super().post(requests, *args, *kwargs)
-
-    # def form_valid(self, form):
-    #     print('*********** FROM VALID ***********')
-
-    #     return super().form_valid(form)
+    success_url = reverse_lazy('autor_app:listar')
 
 
+# Eliminar autor :: delete
 class AutorDeleteView(DeleteView):
     template_name = 'autor/delete_autor.html'
     model = Autor
     fields = ('__all__')
-    success_url = reverse_lazy('autor_app:correcto')
+    success_url = reverse_lazy('autor_app:listar')
